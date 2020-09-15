c5b1585922593ee385542b625ed80d627bc6288b
rapidfireworks <rapidfireworks@gmail.com>
2020-09-15 11:52:45 +0900

    Solve

--- reverse-string/lib/reverse_string.dart
+++ reverse-string/lib/reverse_string.dart
@@ -1,3 +1,10 @@
-String reverse() {
-  // Put your code here
+import 'package:characters/characters.dart';
+
+String reverse(String text) {
+  final characters = text.characters;
+  var result = <String>[];
+  for (var index = characters.length - 1; index >= 0; index -= 1) {
+    result.add(characters.elementAt(index));
+  }
+  return result.join();
 }
