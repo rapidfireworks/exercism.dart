fa2e40547f8527f314c87c6b6d4811279490acb0
rapidfireworks <rapidfireworks@gmail.com>
2020-09-18 09:36:46 +0900

    Refactor without characters

--- reverse-string/lib/reverse_string.dart
+++ reverse-string/lib/reverse_string.dart
@@ -1,10 +1,1 @@
-import 'package:characters/characters.dart';
-
-String reverse(String text) {
-  final characters = text.characters;
-  var result = <String>[];
-  for (var index = characters.length - 1; index >= 0; index -= 1) {
-    result.add(characters.elementAt(index));
-  }
-  return result.join();
-}
+String reverse(String text) => text.split('').reversed.join();
